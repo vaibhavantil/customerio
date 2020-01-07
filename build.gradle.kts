5a8dd721ad0781cc465fb62ad1cea4af4a8f459e
Johan Tjelldén <johan@hedvig.com>
2020-01-07 16:13:32 +0100

    Add bintray publish support

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,13 @@
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.3.41"
-
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 }
+group = "com.hedvig"
+version = "0.2"
 
 repositories {
     // Use jcenter for resolving dependencies.
@@ -38,4 +41,45 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+fun MavenPom.addDependencies() = withXml {
+    asNode().appendNode("dependencies").let { depNode ->
+        configurations.compile.allDependencies.forEach {
+            depNode.appendNode("dependency").apply {
+                appendNode("groupId", it.group)
+                appendNode("artifactId", it.name)
+                appendNode("version", it.version)
+            }
+        }
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    publish = true
+    setPublications("mavenJava")
+    pkg(closureOf<com.jfrog.bintray.gradle.BintrayExtension.PackageConfig> {
+        repo = "hedvig-java"
+        name = "customerio"
+        userOrg = "hedvig"
+        vcsUrl = "https://github.com/HedvigInsurance/customerio.git"
+        setLicenses("MIT")
+    })
 }
